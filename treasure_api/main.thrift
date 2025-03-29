0166afc0ca5bc20d6bbe9cf825eef13ff8ba7f37
xujingshi.0517 <xujingshi.0517@bytedance.com>
2025-03-29 15:35:49 +0800

    u
    Change-Id: Ib0f6a2f69ccb1f127800ad1eb2e20c2f76b6fc61

--- treasure_api/main.thrift
+++ treasure_api/main.thrift
@@ -1,10 +1,18 @@
 namespace go apix
 
-include "entity/page.thrift"
+include "page.thrift"
 
 service treasure {
 
     // 任务页
     page.TaskPageResponse TaskPage(1: page.TaskPageRequest req) (api.get = "/treasure/task/page"),
+
+    /** ------- 任务完成 ------- */
+
+    // 广告任务do_action方法
+    page.AdsDoActionResponse AdsDoAction(1: page.AdsDoActionRequest req) (api.post = "/treasure/task/ads/do_action"),
+
+    // 宝箱任务do_action方法
+    page.BoxDoActionResponse RedPacketRainDoAction(1: page.BoxDoActionRequest req) (api.post = "/treasure/task/box/do_action"),
 
 }
--- treasure_api/page.thrift
+++ treasure_api/page.thrift
@@ -13,3 +13,32 @@ struct TaskPageResponse {
     254: i32 StatusCode (api.body = "status_code"),
     255: common.StatusInfo StatusInfo (api.body = "status_info"),
 }
+
+struct AdsDoActionRequest {
+    1: required common.CommonArgs CommonArgs (api.body = "common_args"),
+    2: required i64 ecpm (api.body = "ecpm"),
+}
+
+struct AdsDoActionResponse {
+    1: BaseDoActionData Data (api.body = "data", go.tag = "json:\"data\""),
+
+    254: i32 StatusCode (api.body = "status_code"),
+    255: common.StatusInfo StatusInfo (api.body = "status_info"),
+}
+
+struct BoxDoActionRequest {
+    1: required common.CommonArgs CommonArgs (api.body = "common_args"),
+}
+
+struct BoxDoActionResponse {
+    1: BaseDoActionData Data (api.body = "data", go.tag = "json:\"data\""),
+
+    254: i32 StatusCode (api.body = "status_code"),
+    255: common.StatusInfo StatusInfo (api.body = "status_info"),
+}
+
+struct BaseDoActionData {
+    1: required list<page.Reward> Rewards (api.body = "rewards"), // 本次发奖任务实例中的奖励信息
+    2: required bool HasNext (api.body = "has_next"),             // 是否有广告任务
+    3: optional page.Task NextTask (api.body = "next_task"),      // 后续广告配置
+}
